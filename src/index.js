ede32e2c07519b0aa87f0da9c7f2c4be050b4636
finley <mafeifan@qq.com>
2017-09-06 17:29:20 +0800

    点击后才加载chunk

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
 // 关于es6的import和export，即便没有安装babel，webpack能够提供开箱即用的支持。
 
-// 我们要动态引入
-// import _ from 'lodash;
+import _ from 'lodash';
 
-function getComponent() {
-  // 这个注释是必要的，根据配置生成的chunk name是lodash.chunk.js，移除注释会变成[id].chunk.js
-  // 而且生成的chunk会在header标签中被引入
-  return import(/* webpackChunkName: "lodash" */ 'lodash')
-  .then(_ => {
-    const element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack!'], ' ');
-    return element;
-  })
-  .catch(error => 'An error occurred while loading the component');
+function component() {
+  
+  const element = document.createElement('div');
+  element.innerHTML = _.join(['Hello', 'webpack!'], ' ');
+
+  const btn = document.createElement('button');
+  btn.innerHTML = 'click me and look at console';
+
+  element.appendChild(btn);
+
+  btn.onclick = e => import(/* webpackChunkName: "print" */ './print')
+	  .then(module => {
+	  	console.log(module);
+	  	// 注意当调用 ES6 模块的 import() 方法（引入模块）时，必须指向模块的 .default 值，
+	  	// 因为它才是 promise 被处理后返回的实际的 module 对象。
+	  	var print = module.default;
+	  	print();
+	  });
+
+  return element;
 }  
  
 
-getComponent().then(element => {
-  document.body.appendChild(element);
-})
+document.body.appendChild(component());
